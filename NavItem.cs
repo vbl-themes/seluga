32755cb04734dfafdf05269db5e4703f813984d5
Ivari Horm <ivari@baltnet.ee>
2023-04-08 18:11:23 +0300

    Initial commit

---
+++ NavItem.cs
@@ -0,0 +1,10 @@
+﻿using Statiq.Common;
+
+namespace Seluga; 
+
+public class NavItem {
+	public IDocument? Previous { get; set; }
+	public IDocument? Next { get; set; }
+	public bool IsPost { get; set; }
+	public bool IsArchive { get; set; }
+}
